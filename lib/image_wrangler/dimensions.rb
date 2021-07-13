1fc30a339bbbb7b53b9603990b6831ce694e6bf7
Tim Davies <tim@eljetico.co.uk>
2021-07-13 01:06:37 +0100

    Add ScalingHelper module and Dimensions class
    This brings scaling calculation into the library

---
+++ lib/image_wrangler/dimensions.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module ImageWrangler
+  class Dimensions
+    attr_accessor :width, :height
+
+    def initialize(width, height)
+      @width = width
+      @height = height
+    end
+
+    def to_a
+      [width, height]
+    end
+
+    def to_s
+      "#{width}x#{height}"
+    end
+  end
+end
---
+++ test/image_wrangler/scaling_helper_test.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require_relative "../test_helper"
+
+module ImageWrangler
+  class ScalingHelperTest < Minitest::Test
+    DummyImage = Struct.new(:width, :height) do
+      include ScalingHelper
+    end
+
+    def setup
+    end
+
+    def test_upscaling_factor
+      subject = DummyImage.new(nil, nil)
+      target_px_area = 5_230_000
+      assert_equal 3.2413, subject.scaling_factor(target_px_area, 990, 503)
+    end
+
+    def test_downscaling_factor
+      subject = DummyImage.new(nil, nil)
+      target_px_area = 1_168_561 # 1080x1080
+      assert_equal 0.3126, subject.scaling_factor(target_px_area, 3000, 4000)
+    end
+
+    def test_upscaling_factor_volume
+      skip("Enable when volume testing")
+      subject = DummyImage.new(nil, nil)
+      target_px_area = 5_230_000
+
+      dims = (100..2286)
+      dims.each do |w|
+        dims.each do |h|
+          scf = subject.scaling_factor(target_px_area, w, h)
+          new_w = (w * scf).ceil
+          new_h = (h * scf).ceil
+          assert((new_w * new_h) >= target_px_area, "#{w} x #{h} @ #{scf} failed")
+        end
+      end
+    end
+
+    def test_downscaling_factor_volume
+      skip("Enable when volume testing")
+      subject = DummyImage.new(nil, nil)
+      target_px_area = 1_168_561
+
+      dims = (2000..5000)
+      dims.each do |w|
+        dims.each do |h|
+          scf = subject.scaling_factor(target_px_area, w, h)
+          new_w = (w * scf).ceil
+          new_h = (h * scf).ceil
+          assert((new_w * new_h) >= target_px_area, "#{w} x #{h} @ #{scf} failed")
+        end
+      end
+    end
+
+    def test_dimensions_for_target_pixel_area
+      subject = DummyImage.new(990, 503)
+      result = subject.dimensions_for_target_pixel_area(5_230_000)
+      assert_equal(3209, result.width)
+      assert_equal(1631, result.height)
+    end
+  end
+end
