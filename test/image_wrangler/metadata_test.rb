372c9cd0f777a12f9b1837d4a6bb7a7a9cec4cb5
Tim Davies <tim@eljetico.co.uk>
2021-07-26 11:58:13 +0100

    WIP - trying to get save tests to work

--- test/image_wrangler/metadata_test.rb
+++ test/image_wrangler/metadata_test.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
 require "mini_exiftool"
+require "tempfile"
+
 require_relative "../test_helper"
 
 module ImageWrangler
   class MetdadataTest < Minitest::Test
     def setup
-      @copy_file = "/tmp/test_write_image.jpg"
+      @temp_file = Tempfile.new("test")
+      @org_filename = raster_path("valid_jpg.jpg")
+      @temp_filename = @temp_file.path
+      FileUtils.cp(@org_filename, @temp_filename)
+      @subject = ImageWrangler::Metadata.new(@temp_filename)
     end
 
     def teardown
-      # File.unlink(@copy_file) if File.exist?(@copy_file)
+      @temp_file.close
     end
 
     def test_ensure_using_local_exiftool
@@ -18,19 +24,13 @@ module ImageWrangler
     end
 
     def test_simple_accessor
-      subject = ImageWrangler::Metadata.new(raster_path("valid_jpg.jpg"))
-      assert_equal "valid_jpg", subject.get_tag("title")
-      assert_equal "Amanda Hall/Robert Harding", subject.get_tag("Credit")
+      assert_equal "valid_jpg", @subject.get_tag("title")
+      assert_equal "Amanda Hall/Robert Harding", @subject.get_tag("Credit")
     end
 
     def test_xmp_tag_write
-      FileUtils.cp(raster_path("valid_jpg.jpg"), @copy_file)
-      # subject = ImageWrangler::Metadata.new(@copy_file)
-      # assert subject.write_tags({"AssetID" => "Timbo"})
-      #
-      # photo = MiniExiftool.new(@copy_file)
-      # assert_equal("Timbo", photo.AssetID)
-      # assert(File.size(@copy_file) > 382000)
+      assert @subject.write_tags({"AssetID" => "Timbo"})
+      assert_equal(2000, File.size(@temp_filename))
     end
   end
 end
